6cab9ed34ea646c10858d62f47fbef4599189beb
efm2324 <gonie.louis@gmail.com>
2024-12-13 10:07:10 +0100

    Update chance_challenges.py

--- chance_challenges.py
+++ chance_challenges.py
@@ -30,3 +30,38 @@ def shell_game():
                 print("reshuffling...")
                 time.sleep(1)
                 print("Try again!")
+                
+def roll_dice_game():
+    attempts= 3
+
+    for attempt in range(attempts):
+        print(f"Attempt {attempt}/{attempts}")
+
+        input("Press 'Enter' to roll the dice.")
+        player = (random.randint(1, 6), random.randint(1, 6))
+        print("You've rolled:... ")
+        time.sleep(1)
+        print(player)
+
+        if 6 in player:
+            print("Congratulations! You have won the game and found the key.")
+            return True
+
+        master = (random.randint(1, 6), random.randint(1, 6))
+        print("Game master rolls:... ")
+        time.sleep(1)
+        print(master)
+
+        if 6 in master:
+            print("The game master has won the game.")
+            return False
+
+        if attempt < attempts-1:
+            print("No one rolled a 6. Moving to the next attempt.\n")
+
+    print("No player scored a 6 after three tries. It's a draw.")
+
+def chanve_challenge():
+    challenges = [shell_game, rollin_dice_game]
+    challenge = random.choice(challenges)
+    return challenge()
