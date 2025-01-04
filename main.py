8fc725317a96725c72a69c23120177f208000ff4
Lehche <gaspardlieske@gmail.com>
2025-01-04 15:42:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-#faire la game()   cf 3.7 docc
+from bato import battleship_game
+from fune import math_challenge
+from luck import chanve_challenge
+from utility import introduction, compose_equipe, challenges_menu, choose_player
+
+
+def game():
+    #introduction and team composition
+    introduction()
+    ListOfPlayers = compose_equipe()
+    Keys = 0
+
+    #event loop
+    Challenge = challenges_menu()
+    Player = choose_player(ListOfPlayers)  #met son prenom uniquement
+
+    print(f"{Player} it is your turn!")
+    if Challenge == 0 :
+        Won = math_challenge()
+    elif Challenge == 1:
+        Won = battleship_game()
+    elif Challenge == 2:
+        Won = chanve_challenge()
+    elif Challenge == 3:
+        #Won = pere_fouras_riddles(file)
+
+    if Won == True :
+        Keys += 1
+        print(f"One more Key for your team, you have {Keys} Keys")
+    else:
+        print(f"No keys for you this time, you have {Keys} Keys")
+
+    
+    #final stage
+    
